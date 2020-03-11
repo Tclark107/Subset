85deb775a5befc8734f94a3548aa761ba360ba7c
tclark107 <tcjc107@gmail.com>
2020-03-11 00:12:54 -0700

    I am finally starting this project

---
+++ Subset.java
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------------------------------------------------------
+// Subset.java
+// Tristan Clark
+//------------------------------------------------------------------------------------------------------------------------------
+
+class Subset{
+
+    static String setToString(int[] B){
+        String a = "abc";
+        return a;
+    }
+
+    static void printSubsets(int[] B, int k, int i) {
+        
+    }
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
