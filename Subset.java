2b1b184b5e2f1723bbc77495fd1f72d897e39841
tclark107 <tcjc107@gmail.com>
2020-03-11 22:06:06 -0700

    error message

--- Subset.java
+++ Subset.java
@@ -5,13 +5,21 @@
 
 class Subset{
 
+    // printErr()
+    // Is a helper function that prints the error message and exits when the user enters the 
+    // wrong thing at the command line.
+    static void printErr(){
+        System.err.println("Usage: Subset <int> <int>");
+        System.exit(0);
+    }
+
     static String setToString(int[] B){
         String a = "abc";
         return a;
     }
 
     static void printSubsets(int[] B, int k, int i) {
-        
+
     }
     public static void main(String[] args){
         System.out.println("hello");
