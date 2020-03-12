dcbf5d851a1c068bf7342e2f64becf1723e53f76
tclark107 <tcjc107@gmail.com>
2020-03-12 13:19:17 -0700

    I now have the correct algorithm

--- Subset.java
+++ Subset.java
@@ -14,14 +14,59 @@ class Subset{
     }
 
     static String setToString(int[] B){
-        String a = "abc";
+        String a = "{";
+        //System.out.print("{");
+        //for(int i = 1; i < B.length; i++){
+        //    System.out.print(B[i]);
+        //}
+        //System.out.println("}");
+        for(int i = 1; i < B.length; i++){
+            if(B[i] == 1){
+                a += i;
+            }
+        }
+        a += "}";
+        //System.out.println("a is: " + a);
         return a;
     }
 
     static void printSubsets(int[] B, int k, int i) {
-
+        //System.out.println("I am on iter " + i);
+        //System.out.println("k = "+k);
+        //System.out.println("B.length: "+B.length);
+        if(k==0){
+            System.out.println(setToString(B));
+            return;
+        }
+        if(k>B.length-i){
+            return;
+        }
+        int[] A = B.clone();
+        A[i] = 1;
+        /*System.out.print("A is: ");
+        for(int x = 1; x < B.length; x++){
+            System.out.print(A[x]);
+        }
+        System.out.println();
+        System.out.print("B is: ");
+        for(int x = 1; x < B.length; x++){
+            System.out.print(B[x]);
+        }
+        System.out.println();*/
+        printSubsets(A, k-1, i+1);
+        printSubsets(B,k,i+1);
     }
     public static void main(String[] args){
-        System.out.println("hello");
+        /*int[] arr = {1,2,3,4};
+        System.out.println(arr);
+        for(int i = 0; i < arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        arr[1] = 10;
+        for(int i = 0; i < arr.length;i++){
+            System.out.print(arr[i]);
+        }*/
+        int[] B = {0,0,0,0};
+        printSubsets(B, 0, 1);
     }
 }
